11c1ee19387b3ef3cb7bd50cde0d71bf6a3af110
Dragonprod <artyom2002-98@mail.ru>
2022-06-05 19:04:17 +0300

    first commit

---
+++ src/main/java/com/example/flowershop/service/FlowerService.java
@@ -0,0 +1,27 @@
+package com.example.flowershop.service;
+
+import com.example.flowershop.entity.Flower;
+import com.example.flowershop.entity.Review;
+import com.example.flowershop.repository.FlowerRepository;
+import com.example.flowershop.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlowerService {
+    @Autowired
+    FlowerRepository flowerRepository;
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    public List<Flower> getFlowers() {
+        return flowerRepository.findAll();
+    }
+
+    public Flower getFlowerById(Long id) {
+        return flowerRepository.findById(id).orElse(null);
+    }
+}
